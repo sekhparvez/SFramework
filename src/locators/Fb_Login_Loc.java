4af0c15261df2d86a59f8c344e9c0529ff838e0d
sekhparvez <46230628+sekhparvez@users.noreply.github.com>
2018-12-28 19:21:18 -0500

    fb login

---
+++ src/locators/Fb_Login_Loc.java
@@ -0,0 +1,11 @@
+package locators;
+
+public class Fb_Login_Loc {
+
+			public String email =      "//input[@id='email']";
+			public String pass =       "//*[@id='pass']";
+			public String login =       "//*[@id='u_0_2']";
+
+
+
+}
---
+++ src/values/Fb_LoginValues.java
@@ -0,0 +1,8 @@
+package values;
+
+public class Fb_LoginValues {
+
+			public String userName = "parvez@gmail.com";
+			public String userPass = "xyz1234";
+
+}
